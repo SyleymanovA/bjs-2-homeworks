96a8dd21bacb3fd4acde00540f62b72a341b70f7
Aziz Suleymanov <9002007000@mail.ru>
2025-02-23 21:20:20 +0500

    function_solution

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,31 @@
-"use strict"
-function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+'use strict';
+function solveEquation (a, b, c){
+    const d= b ** 2 -4 * a * c;
+    let roots = [];
+    if (d<0) {
+        return roots;
+    }
+    if (d===0) {
+        const root = -b/(2*a);
+        roots.push(root);
+        return roots;
+    }
+    else {
+        const root1 = (-b + Math.sqrt(d) )/(2*a);
+        const root2 = (-b - Math.sqrt(d) )/(2*a);
+        roots.push(root1, root2);
+        return roots;
+    }
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let monthlyRate = (percent / 100) / 12;
+  let loanBody = amount - contribution;
+  if (loanBody <= 0) {
+      return 0;
+  }
+  let monthlyPayment = loanBody * (monthlyRate + (monthlyRate / (Math.pow((1 + monthlyRate), countMonths) - 1)));
+  let totalAmount = monthlyPayment * countMonths;
+  return Math.round(totalAmount * 100) / 100;
 }
+
