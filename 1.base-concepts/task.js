5032ce912b5446705920c60b0b9b6b28bb52209c
Aziz Suleymanov <9002007000@mail.ru>
2025-02-25 19:18:15 +0500

    pravki_dz

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -3,19 +3,14 @@ function solveEquation (a, b, c){
     const d= b ** 2 -4 * a * c;
     let roots = [];
     if (d<0) {
-        return roots;
-    }
-    if (d===0) {
+    } else if (d===0) {
         const root = -b/(2*a);
         roots.push(root);
-        return roots;
-    }
-    else {
+    } else {
         const root1 = (-b + Math.sqrt(d) )/(2*a);
         const root2 = (-b - Math.sqrt(d) )/(2*a);
         roots.push(root1, root2);
-        return roots;
-    }
+    } return roots;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
