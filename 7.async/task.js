b49137ea8d0f50ab62ae554d335ba54221662b6d
Aziz Suleymanov <9002007000@mail.ru>
2025-03-19 19:47:36 +0500

    DZ_7

---
+++ 7.async/task.js
@@ -0,0 +1,55 @@
+class AlarmClock {
+    constructor() {
+        this.alarmCollection = [];
+        this.intervalId = null;
+    }
+    addClock(time, callback) {
+        if (!time || !callback) {
+            throw new Error('Отсутствуют обязательные аргументы');
+        }
+        if (this.alarmCollection.some((alarm) => alarm.time === time)) {
+            console.warn('Уже присутствует звонок на это же время');
+            return;
+        }
+        this.alarmCollection.push({
+            callback: callback,
+            time: time,
+            canCall: true,
+        });
+    }
+    removeClock(time) {
+        this.alarmCollection.filter((alarm) => alarm.time !== time);
+    }
+    getCurrentFormattedTime() {
+        const now = new Date();
+        let hours = now.getHours().toString().padStart(2, "0");
+        let minutes = now.getMinutes().toString().padStart(2, "0");
+        return "${hours}:${minutes}";
+    }
+    start() {
+        if (this.intervalId) {
+            return;
+        }
+        this.intervalId = setInterval(() => {
+            this.alarmCollection.forEach((alarm) => {
+                if (alarm.time === this.getCurrentFormattedTime() && alarm.canCall) {
+                    alarm.canCall = false;
+                    alarm.callback();
+                }
+            });
+        }, 1000);
+    }
+    stop() {
+        clearInterval(this.intervalId);
+        this.intervalId = null;
+    }
+    resetAllCalls() {
+        this.alarmCollection.forEach((alarm) => (alarm.canCall = true));
+    }
+    clearAlarms() {
+        this.stop();
+        this.alarmCollection = [];
+    }
+}
+
+
